a4f132005fa54b3a742a5616637005103f2488e2
nick <nick.zwart@gmail.com>
2015-06-12 14:22:02 -0700

    Working autoscale and x/y-limits.

--- display/GPI/Matplotlib_GPI.py
+++ display/GPI/Matplotlib_GPI.py
@@ -54,10 +54,72 @@ class MatplotDisplay(gpi.GenericWidgetGroup):
     def __init__(self, title, parent=None):
         super(MatplotDisplay, self).__init__(title, parent)
 
-        #self.data = self.get_data2()
+        # gpi interface
+        self._collapsables = []
+
+        # plot specific UI side panel
+        #  -sets options for plot window so this needs to be run first
+        vbox = QtGui.QVBoxLayout()
+        vbox.setContentsMargins(0, 0, 0, 0)  # no spaces around this item
+        vbox.setSpacing(0)
+
+        self._autoscale_btn = gpi.widgets.BasicPushButton(self)
+        self._autoscale_btn.set_toggle(True)
+        self._autoscale_btn.set_button_title('autoscale')
+        self._autoscale_btn.valueChanged.connect(self.on_draw)
+        self._collapsables.append(self._autoscale_btn)
+
+        lims = QtGui.QGridLayout()
+        self._xl = gpi.widgets.BasicDoubleSpinBox(self)
+        self._xh = gpi.widgets.BasicDoubleSpinBox(self)
+        self._yl = gpi.widgets.BasicDoubleSpinBox(self)
+        self._yh = gpi.widgets.BasicDoubleSpinBox(self)
+        self._xl.valueChanged.connect(self.on_draw)
+        self._xh.valueChanged.connect(self.on_draw)
+        self._yl.valueChanged.connect(self.on_draw)
+        self._yh.valueChanged.connect(self.on_draw)
+        self._xl.set_immediate(True)
+        self._xh.set_immediate(True)
+        self._yl.set_immediate(True)
+        self._yh.set_immediate(True)
+        self._xlab = QtGui.QLabel('x limits')
+        self._ylab = QtGui.QLabel('y limits')
+        self._maxlab = QtGui.QLabel('max')
+        self._minlab = QtGui.QLabel('min')
+        lims.addWidget(self._maxlab,1,0,1,1)
+        lims.addWidget(self._minlab,2,0,1,1)
+        lims.addWidget(self._xlab,0,1,1,1,alignment=QtCore.Qt.AlignHCenter)
+        lims.addWidget(self._xh,1,1,1,1,alignment=QtCore.Qt.AlignHCenter)
+        lims.addWidget(self._xl,2,1,1,1,alignment=QtCore.Qt.AlignHCenter)
+        lims.addWidget(self._ylab,0,2,1,1,alignment=QtCore.Qt.AlignHCenter)
+        lims.addWidget(self._yh,1,2,1,1,alignment=QtCore.Qt.AlignHCenter)
+        lims.addWidget(self._yl,2,2,1,1,alignment=QtCore.Qt.AlignHCenter)
+        self._collapsables.append(self._xlab)
+        self._collapsables.append(self._ylab)
+        self._collapsables.append(self._xl)
+        self._collapsables.append(self._xh)
+        self._collapsables.append(self._yl)
+        self._collapsables.append(self._yh)
+        self._collapsables.append(self._minlab)
+        self._collapsables.append(self._maxlab)
+
+        vbox.addLayout(lims)
+        vbox.addWidget(self._autoscale_btn)
+        vbox.insertStretch(-1,1)
+
+        # plot window
         self._data = None
-        self.create_main_frame()
+        self._plotwindow = self.create_main_frame()
         self.on_draw()
+
+        # put side panel and plot window together
+        hbox = QtGui.QHBoxLayout()
+        hbox.addLayout(vbox)
+        hbox.addLayout(self._plotwindow)
+        hbox.setStretch(0,11)
+        self.setLayout(hbox)
+
+        self.set_collapsed(True)  # hide options by default
 
     # setters
     def set_val(self, data):
@@ -69,13 +131,50 @@ class MatplotDisplay(gpi.GenericWidgetGroup):
         else:
             return
 
+    def set_autoscale(self, val):
+        self._autoscale_btn.set_val(val)
+        self.on_draw()
+
+    def set_collapsed(self, val):
+        """bool | Only collapse the display options, not the Plot window.
+        """
+        self._isCollapsed = val
+        for wdg in self._collapsables:
+            if hasattr(wdg, 'setVisible'):
+                wdg.setVisible(not val)
+
+    def set_xlim(self, val, quiet=False):
+        '''tuple of floats (min, max)
+        '''
+        self._xh.set_val(val[0])
+        self._xl.set_val(val[1])
+        if not quiet:
+            self.on_draw()
+
+    def set_ylim(self, val, quiet=False):
+        '''tuple of floats (min, max)
+        '''
+        self._yh.set_val(val[0])
+        self._yl.set_val(val[1])
+        if not quiet:
+            self.on_draw()
+
     # getters
     def get_val(self):
         return self._data
 
+    def get_xlim(self):
+        return (self._xh.get_val(), self._xl.get_val())
+
+    def get_ylim(self):
+        return (self._yh.get_val(), self._yl.get_val())
+
+    def get_autoscale(self):
+        return self._autoscale_btn.get_val()
+
     # support
     def create_main_frame(self):
-        self.fig = Figure((6.0, 4.8), dpi=100, facecolor='white', tight_layout=True, linewidth=6.0, edgecolor='0.93')
+        self.fig = Figure((6.0, 4.8), dpi=100, facecolor='white', linewidth=6.0, edgecolor='0.93')
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(self)
         self.canvas.setFocusPolicy(QtCore.Qt.StrongFocus)
@@ -88,14 +187,19 @@ class MatplotDisplay(gpi.GenericWidgetGroup):
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(self.canvas)  # the matplotlib canvas
         vbox.addWidget(self.mpl_toolbar)
-        self.setLayout(vbox)
+        return vbox
+        #self.setLayout(vbox)
 
     #def get_data2(self):
     #    return np.arange(20).reshape([4, 5]).copy()
 
     def on_draw(self):
         self.fig.clear()
-        self.axes = self.fig.add_subplot(111)
+
+        if self.get_autoscale():
+            self.axes = self.fig.add_subplot(111, autoscale_on=self._autoscale_btn.get_val())
+        else:
+            self.axes = self.fig.add_subplot(111, autoscale_on=self._autoscale_btn.get_val(), xlim=self.get_xlim(), ylim=self.get_ylim())
         # self.axes.plot(self.x, self.y, 'ro')
         # self.axes.imshow(self.data, interpolation='nearest')
         # self.axes.plot([1,2,3])
@@ -114,6 +218,10 @@ class MatplotDisplay(gpi.GenericWidgetGroup):
                 self.axes.plot(data[..., 0], data[..., 1], alpha=0.8, lw=2.0)
             else:
                 self.axes.plot(data, alpha=0.8, lw=2.0)
+
+        if self.get_autoscale():
+            self.set_xlim(self.axes.get_xlim(), quiet=True)
+            self.set_ylim(self.axes.get_ylim(), quiet=True)
 
         self.canvas.draw()
 
