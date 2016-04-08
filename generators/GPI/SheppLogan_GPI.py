9c33ce044f2eba0ae4c00ab04554fccb9fbf8d73
ash <aganders3@gmail.com>
2016-04-08 13:41:14 -0700

    Add flip option to SheppLogan node

--- generators/GPI/SheppLogan_GPI.py
+++ generators/GPI/SheppLogan_GPI.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2014, Dignity Health
-# 
+#
 #     The GPI core node library is licensed under
 # either the BSD 3-clause or the LGPL v. 3.
-# 
+#
 #     Under either license, the following additional term applies:
-# 
+#
 #         NO CLINICAL USE.  THE SOFTWARE IS NOT INTENDED FOR COMMERCIAL
 # PURPOSES AND SHOULD BE USED ONLY FOR NON-COMMERCIAL RESEARCH PURPOSES.  THE
 # SOFTWARE MAY NOT IN ANY EVENT BE USED FOR ANY CLINICAL OR DIAGNOSTIC
@@ -13,12 +13,12 @@
 # TO LIFE SUPPORT OR EMERGENCY MEDICAL OPERATIONS OR USES.  LICENSOR MAKES NO
 # WARRANTY AND HAS NOR LIABILITY ARISING FROM ANY USE OF THE SOFTWARE IN ANY
 # HIGH RISK OR STRICT LIABILITY ACTIVITIES.
-# 
+#
 #     If you elect to license the GPI core node library under the LGPL the
 # following applies:
-# 
+#
 #         This file is part of the GPI core node library.
-# 
+#
 #         The GPI core node library is free software: you can redistribute it
 # and/or modify it under the terms of the GNU Lesser General Public License as
 # published by the Free Software Foundation, either version 3 of the License,
@@ -26,7 +26,7 @@
 # in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
 # the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Lesser General Public License for more details.
-# 
+#
 #         You should have received a copy of the GNU Lesser General Public
 # License along with the GPI core node library. If not, see
 # <http://www.gnu.org/licenses/>.
@@ -46,11 +46,12 @@ class ExternalNode(gpi.NodeAPI):
     def execType(self):
         return gpi.GPI_PROCESS
 
-    def initUI(self):      
+    def initUI(self):
 
         # Widgets
         self.addWidget('TextBox', 'Info')
         self.addWidget('SpinBox', 'Size', val=128, min=1, max=8192)
+        self.addWidget('PushButton','Flip', toggle=True, val=True)
         self.addWidget('PushButton','Compute', toggle=True, val=True)
         self.addWidget('SpinBox', 'Bandlimit iterations', val=0, min=0)
 
@@ -58,10 +59,10 @@ class ExternalNode(gpi.NodeAPI):
         self.addOutPort('out', 'NPYarray')
 
     def validate(self):
-        
+
         return 0
-            
-    
+
+
     def compute(self):
 
         #from phantom import phantom
@@ -69,11 +70,13 @@ class ExternalNode(gpi.NodeAPI):
 
         # visibility
         if self.getVal('Compute'):
-        
+
             n = self.getVal('Size')
             out = phantom(n)
 
             out = self.condition(out, itr)
+            if self.getVal('Flip'):
+                out = out[::-1]
 
             self.setData('out', out)
 
@@ -188,26 +191,26 @@ import numpy as np
 def phantom (n = 256, p_type = 'Modified Shepp-Logan', ellipses = None):
 	"""
 	 phantom (n = 256, p_type = 'Modified Shepp-Logan', ellipses = None)
-	
+
 	Create a Shepp-Logan or modified Shepp-Logan phantom.
 
-	A phantom is a known object (either real or purely mathematical) 
-	that is used for testing image reconstruction algorithms.  The 
+	A phantom is a known object (either real or purely mathematical)
+	that is used for testing image reconstruction algorithms.  The
 	Shepp-Logan phantom is a popular mathematical model of a cranial
-	slice, made up of a set of ellipses.  This allows rigorous 
-	testing of computed tomography (CT) algorithms as it can be 
-	analytically transformed with the radon transform (see the 
+	slice, made up of a set of ellipses.  This allows rigorous
+	testing of computed tomography (CT) algorithms as it can be
+	analytically transformed with the radon transform (see the
 	function `radon').
-	
+
 	Inputs
 	------
 	n : The edge length of the square image to be produced.
-	
-	p_type : The type of phantom to produce. Either 
+
+	p_type : The type of phantom to produce. Either
 	  "Modified Shepp-Logan" or "Shepp-Logan".  This is overridden
 	  if `ellipses' is also specified.
-	
-	ellipses : Custom set of ellipses to use.  These should be in 
+
+	ellipses : Custom set of ellipses to use.  These should be in
 	  the form
 	  	[[I, a, b, x0, y0, phi],
 	  	 [I, a, b, x0, y0, phi],
@@ -219,39 +222,39 @@ def phantom (n = 256, p_type = 'Modified Shepp-Logan', ellipses = None):
 	  x0 : Horizontal offset of the centre of the ellipse.
 	  y0 : Vertical offset of the centre of the ellipse.
 	  phi : Counterclockwise rotation of the ellipse in degrees,
-	        measured as the angle between the horizontal axis and 
+	        measured as the angle between the horizontal axis and
 	        the ellipse major axis.
-	  The image bounding box in the algorithm is [-1, -1], [1, 1], 
+	  The image bounding box in the algorithm is [-1, -1], [1, 1],
 	  so the values of a, b, x0, y0 should all be specified with
 	  respect to this box.
-	
+
 	Output
 	------
 	P : A phantom image.
-	
+
 	Usage example
 	-------------
 	  import matplotlib.pyplot as pl
 	  P = phantom ()
 	  pl.imshow (P)
-	
+
 	References
 	----------
-	Shepp, L. A.; Logan, B. F.; Reconstructing Interior Head Tissue 
+	Shepp, L. A.; Logan, B. F.; Reconstructing Interior Head Tissue
 	from X-Ray Transmissions, IEEE Transactions on Nuclear Science,
 	Feb. 1974, p. 232.
-	
-	Toft, P.; "The Radon Transform - Theory and Implementation", 
-	Ph.D. thesis, Department of Mathematical Modelling, Technical 
+
+	Toft, P.; "The Radon Transform - Theory and Implementation",
+	Ph.D. thesis, Department of Mathematical Modelling, Technical
 	University of Denmark, June 1996.
-	
+
 	"""
-	
+
 	if (ellipses is None):
 		ellipses = _select_phantom (p_type)
 	elif (np.size (ellipses, 1) != 6):
 		raise AssertionError ("Wrong number of columns in user phantom")
-	
+
 	# Blank image
 	p = np.zeros ((n, n))
 
@@ -265,18 +268,18 @@ def phantom (n = 256, p_type = 'Modified Shepp-Logan', ellipses = None):
 		x0  = ellip [3]
 		y0  = ellip [4]
 		phi = ellip [5] * np.pi / 180  # Rotation angle in radians
-		
+
 		# Create the offset x and y values for the grid
 		x = xgrid - x0
 		y = ygrid - y0
-		
-		cos_p = np.cos (phi) 
+
+		cos_p = np.cos (phi)
 		sin_p = np.sin (phi)
-		
+
 		# Find the pixels within the ellipse
-		locs = (((x * cos_p + y * sin_p)**2) / a2 
+		locs = (((x * cos_p + y * sin_p)**2) / a2
               + ((y * cos_p - x * sin_p)**2) / b2) <= 1
-		
+
 		# Add the ellipse intensity to those pixels
 		p [locs] += I
 
@@ -290,7 +293,7 @@ def _select_phantom (name):
 		e = _mod_shepp_logan ()
 	else:
 		raise ValueError ("Unknown phantom type: %s" % name)
-	
+
 	return e
 
 
@@ -308,7 +311,7 @@ def _shepp_logan ():
 	        [ .01, .0230, .0460,  .06,  -.605,   0]]
 
 def _mod_shepp_logan ():
-	#  Modified version of Shepp & Logan's head phantom, 
+	#  Modified version of Shepp & Logan's head phantom,
 	#  adjusted to improve contrast.  Taken from Toft.
 	return [[   1,   .69,   .92,    0,      0,   0],
 	        [-.80, .6624, .8740,    0, -.0184,   0],
