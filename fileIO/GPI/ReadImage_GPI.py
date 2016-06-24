855487932e87037423e5144e3f5713d324210cb9
Ashley Anderson <aganders3@gmail.com>
2016-06-24 15:28:22 -0700

    Fix grayscale output of ReadImage: weight colors properly and don't include alpha.

--- fileIO/GPI/ReadImage_GPI.py
+++ fileIO/GPI/ReadImage_GPI.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2014, Dignity Health
-# 
+#
 #     The GPI core node library is licensed under
 # either the BSD 3-clause or the LGPL v. 3.
-# 
+#
 #     Under either license, the following additional term applies:
-# 
+#
 #         NO CLINICAL USE.  THE SOFTWARE IS NOT INTENDED FOR COMMERCIAL
 # PURPOSES AND SHOULD BE USED ONLY FOR NON-COMMERCIAL RESEARCH PURPOSES.  THE
 # SOFTWARE MAY NOT IN ANY EVENT BE USED FOR ANY CLINICAL OR DIAGNOSTIC
@@ -13,12 +13,12 @@
 # TO LIFE SUPPORT OR EMERGENCY MEDICAL OPERATIONS OR USES.  LICENSOR MAKES NO
 # WARRANTY AND HAS NOR LIABILITY ARISING FROM ANY USE OF THE SOFTWARE IN ANY
 # HIGH RISK OR STRICT LIABILITY ACTIVITIES.
-# 
+#
 #     If you elect to license the GPI core node library under the LGPL the
 # following applies:
-# 
+#
 #         This file is part of the GPI core node library.
-# 
+#
 #         The GPI core node library is free software: you can redistribute it
 # and/or modify it under the terms of the GNU Lesser General Public License as
 # published by the Free Software Foundation, either version 3 of the License,
@@ -26,7 +26,7 @@
 # in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
 # the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Lesser General Public License for more details.
-# 
+#
 #         You should have received a copy of the GNU Lesser General Public
 # License along with the GPI core node library. If not, see
 # <http://www.gnu.org/licenses/>.
@@ -42,16 +42,16 @@ import gpi
 class ExternalNode(gpi.NodeAPI):
     """Read images into GPI as numpy arrays. Many image file types are supported by PIL (Python Imaging Library)
     The file types specifically implemented in this node are PNG, JPEG, TIFF and BMP. PIL supports other image
-    file types, but the node's ability to read these types into GPI are not guaranteed. 
-    For more information on supported file types, see 
+    file types, but the node's ability to read these types into GPI are not guaranteed.
+    For more information on supported file types, see
     http://www.pythonware.com/products/pil
     http://effbot.org/imagingbook/pil-index.htm
 
-    OUTPUT: Numpy array read from image file. 
+    OUTPUT: Numpy array read from image file.
 
     WIDGETS: I/O Info - Gives info on data file and type
     File Browser - button to launch file browser, and typein widget if the pathway is known
-    Gray Scale - button to flatten the last dimension of jpg and png images. 
+    Gray Scale - button to flatten the last dimension of jpg and png images.
         If toggle is on, output is a grey-scale float32 array with the same dimensions as the original image
         If toggle is off, output is a uint8 array with the last dimension BGRA
     """
@@ -116,29 +116,29 @@ class ExternalNode(gpi.NodeAPI):
 
         # read the data
         img = Image.open(fname)
-        if img.mode == 'RGB' :
-            img = img.convert("RGBA")
-        imgarr = np.array(img)
-        if flat == True :
-            out = np.mean(imgarr, axis=-1).astype(np.float32)
-        else:
-            if swap == True and imgarr.shape[-1] > 1 :
-                red = imgarr[:,:,2]
-                blu = imgarr[:,:,1]
-                grn = imgarr[:,:,0]
-                if imgarr.shape[-1] == 4 :
-                    alf = imgarr[:,:,3]
-                else :
-                    alf = 255. * np.ones((blu.shape),dtype=np.uint8)
-                
-                out = np.zeros(imgarr.shape,dtype=imgarr.dtype)
-                out[:,:,0] = red
-                out[:,:,1] = blu
-                out[:,:,2] = grn
-                out[:,:,3] = alf
+        if flat:
+            img = img.convert('L')
+        elif img.mode == 'RGB' :
+            img = img.convert('RGBA')
 
+        imgarr = np.array(img)
+
+        if swap and imgarr.shape[-1] > 1 :
+            red = imgarr[:,:,2]
+            blu = imgarr[:,:,1]
+            grn = imgarr[:,:,0]
+            if imgarr.shape[-1] == 4 :
+                alf = imgarr[:,:,3]
             else :
-                out = imgarr
+                alf = 255. * np.ones((blu.shape),dtype=np.uint8)
+
+            out = np.zeros(imgarr.shape,dtype=imgarr.dtype)
+            out[:,:,0] = red
+            out[:,:,1] = blu
+            out[:,:,2] = grn
+            out[:,:,3] = alf
+        else :
+            out = imgarr
 
         d1 = list(out.shape)
         info = "created: "+str(ctime)+"\n" \
