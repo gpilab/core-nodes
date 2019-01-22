99e9639626a874bc38e7cb0cab5b792c45fa9406
Daniel Borup <borup.daniel@mayo.edu>
2019-01-22 16:47:39 -0600

    Modified five nodes and added one
    Modified: Calc101, Math, phaseUnwrap, Rotate, and ToComplex. Added Int_Math to parallel Float_Math.

--- math/GPI/phaseUnwrap_GPI.py
+++ math/GPI/phaseUnwrap_GPI.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2014, Dignity Health
-# 
+#
 #     The GPI core node library is licensed under
 # either the BSD 3-clause or the LGPL v. 3.
-# 
+#
 #     Under either license, the following additional term applies:
-# 
+#
 #         NO CLINICAL USE.  THE SOFTWARE IS NOT INTENDED FOR COMMERCIAL
 # PURPOSES AND SHOULD BE USED ONLY FOR NON-COMMERCIAL RESEARCH PURPOSES.  THE
 # SOFTWARE MAY NOT IN ANY EVENT BE USED FOR ANY CLINICAL OR DIAGNOSTIC
@@ -13,12 +13,12 @@
 # TO LIFE SUPPORT OR EMERGENCY MEDICAL OPERATIONS OR USES.  LICENSOR MAKES NO
 # WARRANTY AND HAS NOR LIABILITY ARISING FROM ANY USE OF THE SOFTWARE IN ANY
 # HIGH RISK OR STRICT LIABILITY ACTIVITIES.
-# 
+#
 #     If you elect to license the GPI core node library under the LGPL the
 # following applies:
-# 
+#
 #         This file is part of the GPI core node library.
-# 
+#
 #         The GPI core node library is free software: you can redistribute it
 # and/or modify it under the terms of the GNU Lesser General Public License as
 # published by the Free Software Foundation, either version 3 of the License,
@@ -26,7 +26,7 @@
 # in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
 # the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Lesser General Public License for more details.
-# 
+#
 #         You should have received a copy of the GNU Lesser General Public
 # License along with the GPI core node library. If not, see
 # <http://www.gnu.org/licenses/>.
@@ -51,7 +51,8 @@ class ExternalNode(gpi.NodeAPI):
 
         # Widgets
         self.addWidget('SpinBox', 'Dimension', val = 0)
-        self.addWidget('ExclusivePushButtons', 'Units',buttons=['degrees','radians'],val=0)
+        self.addWidget('ExclusivePushButtons', 'Units',buttons=
+                        ['degrees','radians','cycles'],val=1)
 
         # IO Ports
         self.addInPort('in', 'NPYarray', dtype=[np.float64, np.float32],
@@ -75,6 +76,9 @@ class ExternalNode(gpi.NodeAPI):
 
         if self.getVal('Units') is 0: #degrees
           out = np.rad2deg(np.unwrap(np.deg2rad(data), discont = np.pi, axis = dim))
+        elif self.getVal('Units') is 2: #cycles
+          out = np.rad2deg(np.unwrap(np.deg2rad(data*360), discont = np.pi,
+                            axis = dim)) / 360
         else: #radians
           out = np.unwrap(data, discont = np.pi, axis = dim)
 
