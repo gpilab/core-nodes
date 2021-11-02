99beef800d4a7fd00b257d7aecbbcce1c40dde80
AbdulRahman <AboudFayad@users.noreply.github.com>
2021-11-02 11:50:00 -0500

    Revert "Revert "Add Image Viewer node""

--- display/GPI/ImageDisplay_GPI.py
+++ display/GPI/ImageDisplay_GPI.py
@@ -323,6 +323,9 @@ class ExternalNode(gpi.NodeAPI):
         # make a copy for changes
         data = self.getData('in').copy()
 
+        # passing the data to viewport widget
+        self.setAttr('Viewport:', data=np.transpose(self.getData('in').copy()))
+
         # get extra dimension parameters and modify data
         dimfunc = self.getVal('Extra Dimension')
         dimval = self.getVal('Slice/Tile Dimension')
