27e6822057b9ebf4349454a778c3b2ea39a134dc
AboudFayad <aboudfayad@outlook.com>
2022-02-09 14:49:48 -0600

    Packaging gpi_core to PyPI

---
+++ setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='gpi_core',
+    version='1.3.2',
+    description="Graphical Programming Interface",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gpilab/framework",
+    author="AbdulRahman Alfayad",
+    author_email="alfayad.abdulrahman@mayo.edu",
+    license="GNU",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.7',
+)
